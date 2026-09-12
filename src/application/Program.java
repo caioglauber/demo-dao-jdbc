a07d873defeb35fc20686f22b6b0ed48f7a0f55c
caioglauber <caioglauber@gmail.com>
2026-09-12 19:13:41 -0300

    delete by id implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,8 +13,11 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
+		/*
 		System.out.println("=== Test 1: findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
@@ -41,8 +45,17 @@ public class Program {
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
-		System.out.println("Up date complete");
-	
+		System.out.println("Update complete");
+		*/
+		
+		System.out.println("\n=== Test 5: delete by Id ===");
+		System.out.print("Enter ID for DELETE: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete Complete!");
+		
+		sc.close();
+		
 	}
 
 }
